52609857452897052ab1bcc21e2bb64bc01a5db9
Imaadh Ifthikar <101471526+imaadh0@users.noreply.github.com>
2026-05-03 12:56:33 +0530

    feat: replace MediaGridOne with OccasionWearCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/LandingPage/Hero";
 import AbayaCarousel from "@/components/LandingPage/AbayaCarousel";
 import CordSetCarousel from "@/components/LandingPage/CordSetCarousel";
-import MediaGridOne from "@/components/LandingPage/MediaGridOne";
+import { OccasionWearCarousel } from "@/components/LandingPage/OccasionWearCarousel";
 import TopsCarousel from "@/components/LandingPage/TopsCarousel";
 import MediaCarousel from "@/components/LandingPage/MediaCarousel";
 
@@ -11,7 +11,7 @@ export default function Home() {
       <Hero />
       <AbayaCarousel />
       <CordSetCarousel />
-      <MediaGridOne />
+      <OccasionWearCarousel />
       <TopsCarousel />
       <MediaCarousel />
     </div>
